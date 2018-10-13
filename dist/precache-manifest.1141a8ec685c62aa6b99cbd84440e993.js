57453738ed46186a3a601b432c488c75b081c67a
erkansivas35 <erkan.sivas35@gmail.com>
2018-10-13 19:06:37 +0300

    heroku added

---
+++ dist/precache-manifest.1141a8ec685c62aa6b99cbd84440e993.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2bdd254b2d14aea9bd6c",
+    "url": "/js/chunk-vendors.f1843bca.js"
+  },
+  {
+    "revision": "4557faa553ed65e4f58c",
+    "url": "/js/app.8275077f.js"
+  },
+  {
+    "revision": "0fdfa7f0fb354ecbed79093ee65c8591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b65676f14807457ee96199e28b2ce02",
+    "url": "/img/place.7b65676f.jpg"
+  },
+  {
+    "revision": "ed52f75d9e4446c9d97f492b25616035",
+    "url": "/img/man.ed52f75d.svg"
+  },
+  {
+    "revision": "4750b8e0e68046a325e598cd117c54d3",
+    "url": "/img/girl.4750b8e0.svg"
+  },
+  {
+    "revision": "3415cb1b8814c6a21ec6127e8e4af0f1",
+    "url": "/img/375935.3415cb1b.jpg"
+  },
+  {
+    "revision": "4557faa553ed65e4f58c",
+    "url": "/css/app.56c31393.css"
+  }
+];
